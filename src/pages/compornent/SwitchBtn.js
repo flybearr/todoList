9e314db78eab9165d43ba17d8ad4c0e9b8bfa078
flybear <goodgood0331@gmail.com>
2023-01-10 22:04:50 +0800

    rwd

--- src/pages/compornent/SwitchBtn.js
+++ src/pages/compornent/SwitchBtn.js
@@ -5,7 +5,8 @@ export default function SwitchBtn({btn,alreadyFinsih}) {
     <div className={styled.switchArea}>
         <p>Move done things to end?</p>
         <div className={styled.switch}>
-          <input className={styled.switchCheckbox} id="switchID1" type="checkbox" name="switch-checkbox" checked={btn} onChange={()=>{alreadyFinsih()}}/>
+          <input className={styled.switchCheckbox} id="switchID1" type="checkbox" name="switch-checkbox" checked={btn} onChange={()=>{alreadyFinsih()
+          }}/>
           <label className={styled.switchLabel} htmlFor="switchID1">
               <span className={styled.switchTxt} turnon="1" turnoff="2"></span>
               <span className={styled.switchRoundBtn}></span>
